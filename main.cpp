ee4719db041cd8d7cd0ff83f6bbf7c024b8b5db7
Bayu Santoso <bayusantoso.mail@gmail.com>
2014-06-05 09:41:54 +0700

    Add Function to Load Object
    Adding Function that can load *.obj file and use texture in *.png, to
    make file obj and texture use blender

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <windows.h>
-#include <GL/glut.h>
 #include <stdlib.h>
-#include <math.h>
 #include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <math.h>
+
+#include <GL/glut.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+
 #include "glm.h"
+#include "lodepng.h"
 
 int xRot = 0, yRot = 0;
 
@@ -12,6 +19,50 @@ float x = 0, y = 3, z = 20;
 float lx = 0, ly = 0, lz = -1;
 GLMmodel* model1 = NULL;
 GLMmodel* model2 = NULL;
+
+// A 3D point.  Used for the scene's geometry.
+typedef struct {
+  float x, y, z;
+} Vertex3D;
+
+// A 2D point.  Used for a scene's texture coordinates on the geometry.
+typedef struct {
+  float x, y;
+} Vertex2D;
+
+// While an Object file may contain multiple objects, each with a set of
+// vertices and texture vertices, all vertex indecies and all texture vertex
+// indecies are unique.  Therefore, we load them in single lists.
+#define MAX_VERTICES 3000
+Vertex3D vertices[MAX_VERTICES];
+int vertex_count = 0;
+Vertex2D texture_vertices[MAX_VERTICES];
+int texture_vertex_count = 0;
+
+// A face is a polygon that makes up part of an object.
+#define MAX_FACE_VERTICES 4
+typedef struct {
+  int vertices[MAX_FACE_VERTICES];
+  int texture_vertices[MAX_FACE_VERTICES];
+  int vertex_count;
+} Face;
+
+// An object is a major part of the scene, such as hair, clothing, and a body.
+typedef struct {
+  #define MAX_NAME_LENGTH 50
+  char name[MAX_NAME_LENGTH];
+
+  #define MAX_FACES 1800
+  Face faces[MAX_FACES];
+  int face_count;
+
+  GLuint texture;
+} Object;
+
+// Objects are parts of the scene, such as hair, body, and clothing.
+#define MAX_OBJECTS 100
+Object objects[MAX_OBJECTS];
+int object_count = 0;
 
 void rotateCamera(float ang) {
     // rotate the camera (left / right)
@@ -105,22 +156,135 @@ void load_BMP_texture(char *filename) {
     free(imdata); // free the texture
 }
 
+void loadObjectWithTexture(char scne_filename[100], char file_path[100]) {
+    glEnable (GL_TEXTURE_2D);
+
+        static const char *SCENE_FILENAME;
+        SCENE_FILENAME = scne_filename;
+      // Open the Object file that contains geometry and texture vertices.
+      // This particular scene is by Peter McAlpine.
+      static const char *READ_MODE = "r";
+      FILE *f = fopen (SCENE_FILENAME, READ_MODE);
+
+      Object *current_object;
+
+      // Read each line of the Object file.
+      static const int MAX_LINE_LENGTH = 100;
+      char line[MAX_LINE_LENGTH];
+      while (fgets (line, MAX_LINE_LENGTH, f) != NULL) {
+        if (strncmp (line, "o ", 2) == 0) { // If starting a new object...
+          current_object = &objects[object_count];
+
+          // Read the object's name.
+          sscanf (line, "o %s", current_object->name);
+          current_object->face_count = 0;
+
+          // Load the PNG as the object's texture.
+          // The texture must be loaded into OpenGL after OpenGL is initialized.
+          static const int MAX_FILENAME_LENGTH = 100;
+          char filename[MAX_FILENAME_LENGTH];
+          sprintf (filename, file_path, current_object->name);
+          unsigned char *buffer, *image;
+          size_t buffer_size, image_size;
+          LodePNG_Decoder decoder;
+          LodePNG_Decoder_init (&decoder);
+          LodePNG_loadFile (&buffer, &buffer_size, filename);
+          LodePNG_Decoder_decode (&decoder, &image, &image_size, buffer, buffer_size);
+          assert (!decoder.error);
+          glGenTextures (1, &current_object->texture);
+          glBindTexture (GL_TEXTURE_2D, current_object->texture);
+          glTexImage2D (GL_TEXTURE_2D, 0, GL_RGB, decoder.infoPng.width, decoder.infoPng.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+          glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+          glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+          free (image);
+          free (buffer);
+          LodePNG_Decoder_cleanup (&decoder);
+
+          object_count++;
+        } else if (strncmp (line, "v ", 2) == 0) { // If describing a vertex...
+          Vertex3D *v = &vertices[vertex_count++];
+          sscanf (line, "v %f %f %f", &v->x, &v->y, &v->z);
+        } else if (strncmp (line, "vt ", 3) == 0) { // If texture vertex...
+          Vertex2D *vt = &texture_vertices[texture_vertex_count++];
+          sscanf (line, "vt %f %f", &vt->x, &vt->y);
+          vt->y = 1 - vt->y;
+        } else if (strncmp (line, "f ", 2) == 0) { // If an object face...
+          Face *face = &current_object->faces[current_object->face_count++];
+          face->vertex_count = 0;
+          char *subline = &line[2];
+          while (subline != NULL) {
+            int vertex_index, texture_vertex_index;
+            sscanf (subline, "%i/%i", &vertex_index, &texture_vertex_index);
+            int fv = face->vertex_count;
+            face->vertices[fv] = vertex_index - 1;
+            face->texture_vertices[fv] = texture_vertex_index - 1;
+            face->vertex_count++;
+            subline = strchr (subline, ' ');
+            if (subline != NULL) {
+              subline = &subline[1];
+            }
+          }
+        } // if type of line
+      } // for each line
+
+      fclose (f);
+
+      int o;
+
+      // Display interesting stats about the loaded Object file.
+      printf ("Vertices: %i\n", vertex_count);
+      printf ("Texture vertices: %i\n", texture_vertex_count);
+      printf ("Objects: %i\n", object_count);
+      for (o = 0; o < object_count; ++o) {
+        printf ("  %s faces: %i\n", objects[o].name, objects[o].face_count);
+      }
+
+      // Start the main program loop.
+      glutMainLoop(); // never returns
+
+      glDisable (GL_TEXTURE_2D);
+
+      // If the main loop ever ended, we'd want to free the textures.
+      for (o = 0; o < object_count; ++o) {
+        glDeleteTextures (1, &objects[o].texture);
+      }
+}
+
 void display() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     glColor3f(0.3, 0.3, 0.3);
     grid_floor(20, 20);
+    // For each object...
+  int o;
+  for (o = 0; o < object_count; ++o) {
+    Object *object = &objects[o];
+    glBindTexture (GL_TEXTURE_2D, object->texture);
 
-    // Your object here
-    //glEnable(GL_TEXTURE_2D);
-    glPushMatrix();
-        glTranslated(0,1,0);
-        glRotatef(90,0,1,0);
-        glScalef(5,5,5);
-        glmDraw(model1, GLM_SMOOTH | GLM_MATERIAL);
-    glPopMatrix();
-    //glDisable(GL_TEXTURE_2D);
+    // For each face of the current object...
+    int f;
+    for (f = 0; f < object->face_count; ++f) {
+      Face *face = &object->faces[f];
+      glPushMatrix();
+          glTranslated(0,4,0);
+          glScalef(0.5,0.5,0.5);
+          glBegin (GL_POLYGON);
+            int v;
+            for (v = 0; v < face->vertex_count; ++v) {
+              // Place a texture coordinate.
+              int texture_vertex_index = face->texture_vertices[v];
+              Vertex2D *texture_vertex = &texture_vertices[texture_vertex_index];
+              glTexCoord2f (texture_vertex->x, texture_vertex->y);
 
+              // Place a geometry vertex.
+              int vertex_index = face->vertices[v];
+              Vertex3D *vertex = &vertices[vertex_index];
+              glVertex3f (vertex->x, vertex->y, vertex->z);
+            }
+          glEnd();
+      glPopMatrix();
+    }
+  }
     glutSwapBuffers();
     glFlush();
 }
@@ -160,11 +324,6 @@ void init() {
     glLoadIdentity();
 
     gluPerspective(45,1,1,100);
-
-    model1 = glmReadOBJ("F:/Kuliah/Semester 2/Komputer Grafis/Tugas Akhir/Project/IPB Baranang Siang Campus Digital Map/final-assignment-ipb-baranang-siang-campus/bin/Debug/model/map1.obj");
-    glmUnitize(model1);
-    glmFacetNormals(model1);
-    glmVertexNormals(model1, 90.0);
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
@@ -228,6 +387,8 @@ int main(int argc, char * * argv) {
 
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LESS);
+
+    loadObjectWithTexture("model/eumi.obj","model/eumi_body.png");
 
     glutMainLoop();
 

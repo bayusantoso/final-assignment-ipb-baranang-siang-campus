eb35f7e8995e57ee5dbd22880016730771f2d35f
Bayu Santoso <bayusantoso.mail@gmail.com>
2014-06-05 08:59:29 +0700

    Add Resources
    Adding Resources That Use For Making 3D Map

--- main.cpp
+++ main.cpp
@@ -2,11 +2,35 @@
 #include <GL/glut.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdio.h>
+#include "glm.h"
 
 int xRot = 0, yRot = 0;
 
-void grid_floor(int baris, int kolom) {
+float cameraAngle = 0.0;
+float x = 0, y = 3, z = 20;
+float lx = 0, ly = 0, lz = -1;
+GLMmodel* model1 = NULL;
+GLMmodel* model2 = NULL;
 
+void rotateCamera(float ang) {
+    // rotate the camera (left / right)
+	lx = sin(ang);
+	lz = -cos(ang);
+	glLoadIdentity();
+	gluLookAt(x, y, z, x + lx, y + ly, z + lz, 0, 1, 0);
+}
+
+void moveCamera(int h_direction, int v_direction) {
+    // move the camera (forward, backward, up, down)
+	x = x + h_direction*(lx)*0.3;
+	z = z + h_direction*(lz)*0.3;
+	y = y + v_direction*0.1;
+	glLoadIdentity();
+	gluLookAt(x, y, z, x + lx, y + ly, z + lz, 0, 1, 0);
+}
+
+void grid_floor(int baris, int kolom) {
     // draw grid on floor (0,0,0)
     int i;
 
@@ -31,11 +55,71 @@ void grid_floor(int baris, int kolom) {
     glEnd();
 }
 
+void load_BMP_texture(char *filename) {
+
+    FILE *file;
+    short int bpp;
+    short int planes;
+    long size;
+    unsigned int texture;
+
+    long imwidth;
+    long imheight;
+    char *imdata;
+
+    file = fopen(filename, "rb");
+    fseek(file, 18, SEEK_CUR);
+
+    fread(&imwidth, 4, 1, file);
+    fread(&imheight, 4, 1, file);
+    size = imwidth * imheight * 3;
+
+    fread(&bpp, 2, 1, file);
+    fread(&planes, 2, 1, file);
+
+    fseek(file, 24, SEEK_CUR);
+    imdata = (char *)malloc(size);
+
+    fread(imdata, size, 1, file);
+
+	char temp;
+    for(long i = 0; i < size; i+=3){
+        temp = imdata[i];
+        imdata[i] = imdata[i+2];
+        imdata[i+2] = temp;
+    }
+
+    fclose(file);
+
+    glGenTextures(1, &texture); // then we need to tell OpenGL that we are generating a texture
+    glBindTexture(GL_TEXTURE_2D, texture); // now we bind the texture that we are working with
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, imwidth, imheight, 0, GL_RGB, GL_UNSIGNED_BYTE, imdata);
+
+    free(imdata); // free the texture
+}
+
 void display() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     glColor3f(0.3, 0.3, 0.3);
     grid_floor(20, 20);
+
+    // Your object here
+    //glEnable(GL_TEXTURE_2D);
+    glPushMatrix();
+        glTranslated(0,1,0);
+        glRotatef(90,0,1,0);
+        glScalef(5,5,5);
+        glmDraw(model1, GLM_SMOOTH | GLM_MATERIAL);
+    glPopMatrix();
+    //glDisable(GL_TEXTURE_2D);
 
     glutSwapBuffers();
     glFlush();
@@ -44,10 +128,26 @@ void display() {
 void pressKey(int key, int x, int y) {
 
     switch (key) {
-        case GLUT_KEY_LEFT  : yRot -= 1;break;
-        case GLUT_KEY_RIGHT : yRot += 1;break;
-        case GLUT_KEY_UP    : xRot -= 1;break;
-        case GLUT_KEY_DOWN  : xRot += 1;break;
+	    case GLUT_KEY_LEFT :
+            cameraAngle -= 0.02;
+            rotateCamera(cameraAngle);
+            break;
+        case GLUT_KEY_RIGHT :
+            cameraAngle += 0.02;
+            rotateCamera(cameraAngle);
+            break;
+        case GLUT_KEY_UP :
+            moveCamera(1, 0);
+            break;
+        case GLUT_KEY_DOWN :
+            moveCamera(-1, 0);
+            break;
+        case GLUT_KEY_PAGE_UP :
+            moveCamera(0, 1);
+            break;
+        case GLUT_KEY_PAGE_DOWN :
+            moveCamera(0, -1);
+            break;
     }
 
     glutPostRedisplay();
@@ -60,35 +160,51 @@ void init() {
     glLoadIdentity();
 
     gluPerspective(45,1,1,100);
-    gluLookAt(0, 5, 20, 0, 0, 0, 0.0f,1.0f,0.0f);
+
+    model1 = glmReadOBJ("F:/Kuliah/Semester 2/Komputer Grafis/Tugas Akhir/Project/IPB Baranang Siang Campus Digital Map/final-assignment-ipb-baranang-siang-campus/bin/Debug/model/map1.obj");
+    glmUnitize(model1);
+    glmFacetNormals(model1);
+    glmVertexNormals(model1, 90.0);
 
     glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(x, y, z, x + lx, y + ly, z + lz, 0, 1, 0);
+
 }
 
 void lighting(){
-    // Fungsi mengaktifkan pencahayaan
-    // Pembahasan di materi selanjutnya
 
-    const GLfloat light_ambient[]  = { 0.5f, 0.5f, 0.5f, 1.0f };
-    const GLfloat light_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };
-    const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-    const GLfloat light_position[] = { -2.0f, 3.0f, 5.0f, 1.0f };
-
-    const GLfloat mat_ambient[]    = { 0.7f, 0.7f, 0.7f, 1.0f };
-    const GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f };
-    const GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
-    const GLfloat high_shininess[] = { 100.0f };
-
-    glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_LESS);
-    glEnable(GL_LIGHT0);
-    glEnable(GL_NORMALIZE);
-    glEnable(GL_COLOR_MATERIAL);
     glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glEnable(GL_LIGHT1);
+    glEnable(GL_NORMALIZE);
+
+    float light_ambient[]  = { 1, 1, 1, 1.0 };
+    float light_diffuse[]  = { 1.0, 1.0, 1.0, 1.0 };
+    float light_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+    float light_position[] = {0.0, 0.0, 50.0, 0.0 };
+
     glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient);
     glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse);
     glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
     glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+    float light_position_1[] = {0.0, 10.0, 0.0, 0.0 };
+    glLightfv(GL_LIGHT1, GL_AMBIENT,  light_ambient);
+    glLightfv(GL_LIGHT1, GL_DIFFUSE,  light_diffuse);
+    glLightfv(GL_LIGHT1, GL_SPECULAR, light_specular);
+    glLightfv(GL_LIGHT1, GL_POSITION, light_position_1);
+    /*glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, 2.0);
+    glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, 1.0);
+    glLightf(GL_LIGHT0, GL_QUADRATIC_ATTENUATION, 0.5);*/
+
+    float mat_ambient[]    = { 1.0, 1.0, 1.0, 1.0 };
+    float mat_diffuse[]    = { 1.0, 1.0, 1.0, 1.0 };
+    float mat_specular[]   = { 1.0, 1.0, 1.0, 1.0 };
+    float high_shininess[] = { 25.0 };
+
+    glEnable(GL_COLOR_MATERIAL);
+
     glMaterialfv(GL_FRONT, GL_AMBIENT,   mat_ambient);
     glMaterialfv(GL_FRONT, GL_DIFFUSE,   mat_diffuse);
     glMaterialfv(GL_FRONT, GL_SPECULAR,  mat_specular);
@@ -102,12 +218,12 @@ int main(int argc, char * * argv) {
 
     glutInitWindowSize(500, 500);
     glutInitWindowPosition(100, 100);
-    glutCreateWindow("3D Objects");
+    glutCreateWindow("Texture Mapping");
 
     glutDisplayFunc(display);
     glutSpecialFunc(pressKey);
-    init();
 
+    init();
     lighting();
 
     glEnable(GL_DEPTH_TEST);
